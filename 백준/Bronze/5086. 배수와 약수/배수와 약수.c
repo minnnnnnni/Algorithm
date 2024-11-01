8b8bf783a45f8b939c9f5909da80b7a8e3a4a2f8
minnnnnnni <135509721+minnnnnnni@users.noreply.github.com>
2024-11-01 15:00:08 +0900

    [Bronze III] Title: 배수와 약수, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+    int a,b;
+    
+    while(1) {
+        scanf("%d %d", &a, &b);
+        if (a==0 && b==0) // 0이 두 번 입력되면 반복문 종료
+            break;
+        if (a%b==a && b%a==0) {
+            printf("factor\n");
+        } else if (a%b==0 && b%a==b) {
+            printf("multiple\n");
+        } else if (a%b) {
+            printf("neither\n");
+        }
+    }
+    return 0;
+}
