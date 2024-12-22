3630c22c764a4dbae5f9c67f0fad727a80671bff
minnnnnnni <135509721+minnnnnnni@users.noreply.github.com>
2024-12-22 21:50:31 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+    int index;
+    char s[1001];
+    scanf("%s", s);
+    scanf("%d", &index);
+    
+    printf("%c", s[index-1]);
+    
+    return 0;
+}
